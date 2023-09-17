cbc59b34b1550cd7215dd900c014b9c9dc03fdeb
aurwei <145221230+aurwei@users.noreply.github.com>
2023-09-17 11:43:00 +0200

    🧪 Add bot testing

---
+++ src/tests/bot.test.ts
@@ -0,0 +1,27 @@
+import { afterAll, describe, expect, test } from '@jest/globals';
+import { Client } from 'discord.js';
+
+describe('bot', () => {
+	const bot = new Bot();
+
+	describe('start up', () => {
+
+		test('client should be Client type', () => {
+			expect(bot.client).toBeInstanceOf(Client);
+		});
+
+		test('client should have Guilds intent', () => {
+			expect(bot).toHaveProperty('client.options.intents.bitfield', 1);
+		});
+
+		test('should return bot token', async () => {
+			const botToken = process.env.BOT_TOKEN;
+			expect((await bot.login())).toBe(botToken);
+		});
+	});
+
+	afterAll(() => {
+		bot.destroy();
+	});
+
+});
