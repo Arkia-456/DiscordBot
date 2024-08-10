a4ad2bd1d0c6c29cc430d456f261d8b129cbea9e
Arkia <145221230+Arkia-456@users.noreply.github.com>
2024-08-10 14:34:51 +0200

    Menu: Add cron to generate menu each week

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import dotenv from 'dotenv';
 import CookingDatabase from './app/core/database/cooking/CookingDatabase';
 import { Bot } from './app/core/bot/Bot';
 import { ApplicationFatalError } from './app/core/utils/error/ApplicationFatalError';
+import cron from 'node-cron';
+import { Menu } from './app/core/database/cooking/models/Menu';
+
 
 async function main() {
 	dotenv.config();
@@ -13,6 +16,29 @@ async function main() {
 
 		// At init, check if menu exist
 		if (!(await Menu.getNextWeekMenu())) await Menu.createWeekMenu();
+
+		// Generate weekly menu
+		const schedule = '* * 8 * * Thursday';
+		const menuTask = cron.schedule(schedule, async () => {
+			if (!(await Menu.getNextWeekMenu())) await Menu.createWeekMenu();
+		});
+		menuTask.start();
+
+		const guildIds = process.env.PRIVATE_GUILD_IDS;
+		if (guildIds) {
+			const [guildId] = guildIds.split(',');
+			if (guildId) {
+				const guild = await bot.client.guilds.fetch(guildId);
+				if (guild) {
+					const channels = await guild.channels.fetch();
+					const channel = channels.find(c => c?.name === 'menus');
+					if (channel && channel.isTextBased()) {
+						await Bot.sendWeeklyMenu(channel);
+					}
+				}
+			}
+		}
+
 	} catch (error) {
 		if (error instanceof ApplicationFatalError) {
 			await bot.destroy();
